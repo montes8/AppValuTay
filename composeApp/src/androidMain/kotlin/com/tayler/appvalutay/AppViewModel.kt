cadf7d26ad954e5006073eda927ec4e07aabd182
Tayler <emontesinos812@gmail.com>
2025-08-23 07:17:31 -0500

    test userUsescases

--- composeApp/src/androidMain/kotlin/com/tayler/appvalutay/AppViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/tayler/appvalutay/AppViewModel.kt
@@ -1,14 +1,15 @@
 package com.tayler.appvalutay
 
 import androidx.lifecycle.ViewModel
+import com.tayler.appvalutay.casesuses.UserCaseUse
 import com.tayler.appvalutay.casesuses.network.IUserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class AppViewModel(private val repository: IUserRepository) : ViewModel() {
+class AppViewModel(private val repository: UserCaseUse) : ViewModel() {
 
      val text = MutableStateFlow("")
     fun sayHello(name : String){
-        val foundUser = repository.findUser(name)
+        val foundUser = repository.sayHello(name)
         text.value = foundUser?.let { "Hello '$it'" } ?: "User '$name' not found!"
     }
 }
