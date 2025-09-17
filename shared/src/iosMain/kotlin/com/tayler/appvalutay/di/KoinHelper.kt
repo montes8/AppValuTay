a5f6ccabb98c749f95e62dc511f5ef5a55425570
tayler <emontesinos812@gmail.com>
2025-09-17 09:11:47 -0500

    config koin ios

--- shared/src/iosMain/kotlin/com/tayler/appvalutay/di/KoinHelper.kt
+++ shared/src/iosMain/kotlin/com/tayler/appvalutay/di/KoinHelper.kt
@@ -1,16 +1,25 @@
 package com.tayler.appvalutay.di
 
+import com.tayler.appvalutay.manager.db.platformModule
+import com.tayler.appvalutay.model.LocationModel
+import com.tayler.appvalutay.model.UserModel
 import com.tayler.appvalutay.usecases.AppCaseUse
+import com.tayler.appvalutay.usecases.DataUseCase
+import com.tayler.appvalutay.usecases.UserCaseUse
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.koin.core.context.startKoin
 
 class KoinHelper : KoinComponent {
     private val appCaseUse : AppCaseUse by inject()
+    private val userCaseUse : UserCaseUse by inject()
+    private val dataUseCase : DataUseCase by inject()
     fun getSession() : Boolean = appCaseUse.getSession()
+
+    suspend fun saveUser() : Boolean = userCaseUse.saveUser()
+    suspend fun getUser() : UserModel = userCaseUse.getUser()
+    suspend fun loadLocations() : List<LocationModel> = dataUseCase.loadLocations()
 }
 
 fun initKoinIos() = initKoin(appDeclaration = {
-    modules(appModule,networkModule)
-
+    modules(appModule,networkModule,dbModule(), platformModule())
 })
